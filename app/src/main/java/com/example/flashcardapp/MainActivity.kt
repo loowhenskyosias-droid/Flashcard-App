76b57b744848e874189d1bb64e14239934d885c2
Schnaider Osias <loowhenskyosias@gmail.com>
2025-11-15 18:35:29 -0500

    adding linstener on flashcard question

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.flashcardapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val flashcardquestion = findViewById<TextView>( R.id.flashcardquestion)
+        val flashcardanswer = findViewById<TextView>( R.id.flashcardanswer)
+        flashcardquestion.setOnClickListener{
+            flashcardquestion .visibility = View .INVISIBLE
+            flashcardanswer .visibility = View .VISIBLE
         }
     }
 }
